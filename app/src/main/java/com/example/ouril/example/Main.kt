b3d5f9819484d83ab88fa5af917012819bc1427a
ouril <ourilkregg@gmail.com>
2018-03-30 23:44:23 +0300

    first commit

---
+++ app/src/main/java/com/example/ouril/example/Main.kt
@@ -0,0 +1,25 @@
+package com.example.ouril.example
+
+import android.os.Bundle;
+import android.os.PersistableBundle
+import android.support.v7.app.AppCompatActivity;
+import android.view.ActionMode;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AbsListView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.PopupMenu;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+class MainAplication: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+    }
+}
