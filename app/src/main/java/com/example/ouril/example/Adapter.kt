83b8595842f1209c9e7d2e80963c46713dbaf0de
ouril <ourilkregg@gmail.com>
2018-04-02 15:34:12 +0300

    added remove item

--- app/src/main/java/com/example/ouril/example/Adapter.kt
+++ app/src/main/java/com/example/ouril/example/Adapter.kt
@@ -9,6 +9,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import com.example.ouril.example.models.CityWhether
+import com.example.ouril.example.models.Whether
 import kotlinx.android.synthetic.main.item_city.view.*
 import javax.security.auth.callback.Callback
 
@@ -18,36 +19,38 @@ interface WhetherAdapterClickListener {
     fun removeView(position:Int)
 
     fun editView(position: Int)
+
+    fun addView(text: String, whether: Whether)
 }
 
 
 class WhetherAdapter(private val myDataset: ArrayList<CityWhether>) :
         RecyclerView.Adapter<WhetherAdapter.ViewHolder>(), WhetherAdapterClickListener {
-    override fun removeView(position: Int){
 
+    override fun removeView(position: Int){
+        myDataset.removeAt(position)
+        notifyDataSetChanged()
     }
 
     override fun editView(position: Int) {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
+    override fun addView(text: String, whether: Whether) {
+        val cityWhether = CityWhether(text, whether)
+        myDataset.add(cityWhether)
+        notifyDataSetChanged()
+    }
 
-
-    class ViewHolder(val textView: View): RecyclerView.ViewHolder(textView),
+    inner class ViewHolder(val textView: View): RecyclerView.ViewHolder(textView),
             View.OnClickListener,
             PopupMenu.OnMenuItemClickListener{
 
-
         val mTextView = textView.findViewById(R.id.txtTitle) as TextView
         val mContextView = textView.findViewById(R.id.txtOptionDigit) as TextView
+
         init {
             mContextView.setOnClickListener(this)
-        }
-
-        fun bind(text: String, callbacks: WhetherAdapterClickListener){
-            //this = callbacks
-            mTextView.setText(text)
-
         }
 
         override fun onClick(v: View?) {
@@ -67,16 +70,14 @@ class WhetherAdapter(private val myDataset: ArrayList<CityWhether>) :
                     return true
                 }
                 R.id.menu_delete -> {
-                    return false
+                    removeView(this.adapterPosition)
+                    return true
+
                 }
             }
             return false
         }
-
-
     }
-
-
 
     override fun onCreateViewHolder(parent: ViewGroup,
                                     viewType: Int): WhetherAdapter.ViewHolder {
