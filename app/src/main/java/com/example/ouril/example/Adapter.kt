47c51eaa0e7e97855be4fc9c22b4d436f98cbcc8
ouril <ourilkregg@gmail.com>
2018-04-02 14:10:50 +0300

    added popup

--- app/src/main/java/com/example/ouril/example/Adapter.kt
+++ app/src/main/java/com/example/ouril/example/Adapter.kt
@@ -1,5 +1,6 @@
 package com.example.ouril.example
 
+import android.support.v7.widget.CardView
 import android.support.v7.widget.PopupMenu
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
@@ -9,6 +10,7 @@ import android.view.ViewGroup
 import android.widget.TextView
 import com.example.ouril.example.models.CityWhether
 import kotlinx.android.synthetic.main.item_city.view.*
+import javax.security.auth.callback.Callback
 
 
 interface WhetherAdapterClickListener {
@@ -20,18 +22,61 @@ interface WhetherAdapterClickListener {
 
 
 class WhetherAdapter(private val myDataset: ArrayList<CityWhether>) :
-        RecyclerView.Adapter<WhetherAdapter.ViewHolder>() {
-
-    class ViewHolder(val textView: View): RecyclerView.ViewHolder(textView), WhetherAdapterClickListener {
-        override fun removeView(position: Int){
-
-        }
-
-        override fun editView(position: Int) {
-            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-        }
+        RecyclerView.Adapter<WhetherAdapter.ViewHolder>(), WhetherAdapterClickListener {
+    override fun removeView(position: Int){
 
     }
+
+    override fun editView(position: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+
+
+    class ViewHolder(val textView: View): RecyclerView.ViewHolder(textView),
+            View.OnClickListener,
+            PopupMenu.OnMenuItemClickListener{
+
+
+        val mTextView = textView.findViewById(R.id.txtTitle) as TextView
+        val mContextView = textView.findViewById(R.id.txtOptionDigit) as TextView
+        init {
+            mContextView.setOnClickListener(this)
+        }
+
+        fun bind(text: String, callbacks: WhetherAdapterClickListener){
+            //this = callbacks
+            mTextView.setText(text)
+
+        }
+
+        override fun onClick(v: View?) {
+            when(v!!.id) {
+                R.id.txtOptionDigit -> {
+                    val popup = PopupMenu(v.context, v)
+                    popup.inflate(R.menu.contex_menu)
+                    popup.setOnMenuItemClickListener(this)
+                    popup.show()
+                }
+            }
+        }
+
+        override fun onMenuItemClick(item: MenuItem?): Boolean {
+            when(item!!.itemId) {
+                R.id.menu_edit -> {
+                    return true
+                }
+                R.id.menu_delete -> {
+                    return false
+                }
+            }
+            return false
+        }
+
+
+    }
+
+
 
     override fun onCreateViewHolder(parent: ViewGroup,
                                     viewType: Int): WhetherAdapter.ViewHolder {
@@ -45,6 +90,7 @@ class WhetherAdapter(private val myDataset: ArrayList<CityWhether>) :
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
 
         holder.textView.txtTitle.text = myDataset[position].name
+        holder.textView.txtWhether.text = myDataset[position].whether.toString()
     }
     
     override fun getItemCount() = myDataset.size
